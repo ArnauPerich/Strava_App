e6d5c68419e4933a4691ef85ffa87db6e7df545a
Arnau Perich Iglesias <arnau.perichiglesias@gmail.com>
2026-06-04 21:12:18 +0200

    init_db() fix

--- backend/app.py
+++ backend/app.py
@@ -378,7 +378,8 @@ def on_connect():
         ensure_polling(athlete_id)
 
 
+init_db()
+
 if __name__ == "__main__":
-    init_db()
     socketio.run(app, debug=True, use_reloader=False,
                  host="0.0.0.0", port=5000, allow_unsafe_werkzeug=True)
